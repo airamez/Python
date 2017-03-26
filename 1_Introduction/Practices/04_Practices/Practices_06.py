2459a81dde324f84f0bd1b649c3060b7f821de2a
airamez <airamez@gmail.com>
2017-03-26 13:23:35 -0700

    Bug fixes and improvements

--- 1_Introduction/Practices/04_Practices/Practices_06.py
+++ 1_Introduction/Practices/04_Practices/Practices_06.py
@@ -1,3 +1,9 @@
 """
 Read an temperature value in Fahrenheit and calculate it correspondent value in Celsius.
+Source: http://www.rapidtables.com/convert/temperature/how-fahrenheit-to-celsius.htm
+Formula: T(°C) = (T(°F) - 32) / 1.8
 """
+
+Fahrenheit = int(input("Fahrenheit = "))
+Celsius = round((Fahrenheit - 32) / 1.8, 2)
+print("Celsius", Celsius, "°C")
--- 1_Introduction/Practices/04_Practices/Practices_07.py
+++ 1_Introduction/Practices/04_Practices/Practices_07.py
@@ -1,3 +1,13 @@
+import math
+
 """
 Read the two sides of a triangle and calculate the hypotenuse.
+h2 = a2 + b2
 """
+a = int(input("Side A: "))
+b = int(input("Side B: "))
+
+h = math.sqrt((math.pow(a, 2) + math.pow(b, 2)))
+
+print("Hypotenuse = ", h)
+
