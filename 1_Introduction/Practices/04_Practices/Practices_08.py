37a72cd55c701edd36efc524e6794c112e5f45c6
airamez <airamez@gmail.com>
2017-04-01 14:44:44 -0700

    Bug fixes and improvements

--- 1_Introduction/Practices/04_Practices/Practices_08.py
+++ 1_Introduction/Practices/04_Practices/Practices_08.py
@@ -1,3 +1,25 @@
 """
 Read two integer numbers storing each one in a variable and switch the variable values.
 """
+
+# Method 1
+number1 = int(input("Number 1 = "))
+number2 = int(input("Number 2 = "))
+
+aux = number1
+number1 = number2
+number2 = aux
+
+print("Number 1 = ", number1)
+print("Number 2 = ", number2)
+
+# Method 2
+number1 = int(input("Number 1 = "))
+number2 = int(input("Number 2 = "))
+
+number1, number2 = number2, number1
+
+print("Number 1 = ", number1)
+print("Number 2 = ", number2)
+
+
--- 1_Introduction/Practices/04_Practices/Practices_09.py
+++ 1_Introduction/Practices/04_Practices/Practices_09.py
@@ -1,9 +1,17 @@
 """
-Read the purchase price and desired profit percentual (%) for a product and calculate the sale price.
+Read the purchase price and desired profit percentage(%) for a product and calculate the sale price.
     e.g.
     INPUT
       Purchase Price = 500.00
-      Desired Profit (%) = 50
+      Desired Profit (%) = 30
     OUTPUT
-      Sale Price = 750.00
+      Sale Price = 650.00
 """
+purchase_price = float(input("Purchase Price: "))
+desired_profit = float(input("Desired Profit (%): "))
+
+profit = purchase_price * (desired_profit / 100)
+sale_price = purchase_price + profit
+
+print("Profit =", profit)
+print("Sale price = ", sale_price)
