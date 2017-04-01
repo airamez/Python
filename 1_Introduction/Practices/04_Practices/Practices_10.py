6bf9ec315bf31511a2c32cae95a0aa376c56fac1
airamez <airamez@gmail.com>
2017-04-01 14:57:36 -0700

    Bug fixes and improvements

--- 1_Introduction/Practices/04_Practices/Practices_10.py
+++ 1_Introduction/Practices/04_Practices/Practices_10.py
@@ -1,4 +1,11 @@
 """
 Calculate how many minutes a client stayed in a store based on the time (Hour:Minutes) he arrived and the
     time (Hour:Minutes) he left.
+    e.g
+    INPUT
+    Arrived at Hour = 8
+    Arrive at Minutes = 15
+    Left at Hour = 9
+    Left at Minute = 5
+    Minutes in store = 50 minutes
 """
