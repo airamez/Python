1e0850749eeaf377c7e0ff4b0a8f9bbd4df53f05
airamez <airamez@gmail.com>
2017-04-01 13:50:32 -0700

    Bug fixes and improvements

---
+++ 1_Introduction/Practices/04_Practices/Practices_11.py
@@ -0,0 +1,13 @@
+"""
+Calculate how many dollar's bills of each one of the following values ($1, $5, $10, $20, $50) should be released
+    in an ATM machine for a withdraw value.
+    The ATM machine should always give less dollar bills as possible
+    e.g. INPUT
+      Value = 1,338.00
+    OUTPUT
+      50 bills = 26
+      20 bills = 1
+      10 bills = 1
+       5 bills = 1
+       1 bills = 3
+"""
