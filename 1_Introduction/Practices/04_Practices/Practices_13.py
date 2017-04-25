4581f94ee5f73b16c521e6f478d7a85b840cdeea
airamez <airamez@gmail.com>
2017-04-24 21:56:26 -0700

    Bug fixes and improvements

--- 1_Introduction/Practices/04_Practices/Practices_13.py
+++ 1_Introduction/Practices/04_Practices/Practices_13.py
@@ -1,3 +1,6 @@
 """
-Read an integer number and indicate if the number is even.
+Read an integer number and indicate if the number is even or odd.
 """
+number = int(input("Number = "))
+remainder = number % 2
+print("Even if Zero; Odd if One = ", remainder)
