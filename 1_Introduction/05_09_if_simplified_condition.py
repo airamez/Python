5d93b1ca8c7a6d52a1b8bb5410aca4bd8349b9d6
airamez <airamez@gmail.com>
2017-05-04 23:31:47 -0700

    Bug fixes and improvements

--- 1_Introduction/05_09_if_simplified_condition.py
+++ 1_Introduction/05_09_if_simplified_condition.py
@@ -9,17 +9,19 @@ right = random.randint(left + 1, 100)
 number = int(input("Type a number between 0 and 100 = "))
 print("Interval [{0}..{1}]".format(left, right))
 
+# Method 1
 if left <= number and number <= right:
     print("Number inside the the interval!")
 else:
     print("Number is outside the interval!")
 
-# Simplified
+# Method 2: Simplified
 if left <= number <= right:
     print("Number inside the the interval!")
 else:
     print("Number is outside the interval!")
 
+# Method 3: Inverted logical conditions
 if number < left or number > right:
     print("Number is outside the interval!")
 else:
