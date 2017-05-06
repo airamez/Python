89aad2a08bd1b4f298d7f6f5ad171735223cf17f
Jose Santos <airamez@gmail.com>
2017-05-05 23:17:39 -0700

    Fixing errors and improving the content

--- 1_Introduction/07_Function.py
+++ 1_Introduction/07_Function.py
@@ -28,7 +28,7 @@ print('This is a function')
 print(repeat('=', 80))
 
 
-def get_salutation(name: str, hour: int) -> str:
+def get_salutation(name: str, hour: int) -> str:  # Providing the type of the parameters and return type
     """
     This function returns the proper salutation based on the hour
     :param name: Name
@@ -50,6 +50,8 @@ print(get_salutation("José", 8))
 print(get_salutation("Artur", 13))
 print(get_salutation("Leila", 18))
 print(get_salutation("Nilzete", 22))
+print(get_salutation(name="Alciene", hour=7))
+print(get_salutation(hour=15, name="Zenilton"))
 
 myName = "José"
 myHour = 15
