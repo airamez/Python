3e1cbf68ec12f523b50b2a6c4cd1f4913c0d8686
airamez <airamez@gmail.com>
2017-05-07 13:01:56 -0700

    Bug fixes and improvements

--- 1_Introduction/Practices/05_Practices/Practices_02.py
+++ 1_Introduction/Practices/05_Practices/Practices_02.py
@@ -1,3 +1,29 @@
 """
-
+Read two integer numbers and inform if the numbers are equals or different.
 """
+number1 = int(input("Number 1 = "))
+number2 = int(input("Number 2 = "))
+
+# Option 1
+if number1 == number2:
+    print("Equals")
+else:
+    print("Different")
+
+# Option 2
+if number1 != number2:
+    print("Different")
+else:
+    print("Equals")
+
+# Option 3
+if not number1 != number2:
+    print("Equals")
+else:
+    print("Different")
+
+# Option 4
+if not number1 == number2:
+    print("Different")
+else:
+    print("Equals")
