8cee750be52fdc96f92a8ac9f5f19ce5a86d4c78
airamez <airamez@gmail.com>
2017-05-09 00:03:37 -0700

    Bug fixes and improvements

--- 1_Introduction/Practices/05_Practices/Practices_03.py
+++ 1_Introduction/Practices/05_Practices/Practices_03.py
@@ -1,3 +1,29 @@
 """
-
+Read a number and inform if the number is positive, negative or zero.
 """
+
+number = float(input("Number = "))
+
+# Option 1
+if number == 0:
+    print("Zero")
+elif number > 0:
+    print("Positive")
+else:
+    print("Negative")
+
+# Option 2
+if number > 0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+# Option 3
+if number < 0:
+    print("Negative")
+elif number == 0:
+    print("Zero")
+else:
+    print("Positive")
