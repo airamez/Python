00045200c80a1521f9bf3f5cc3c07cec451cb264
airamez <airamez@gmail.com>
2017-05-09 22:03:48 -0700

    Bug fixes and improvements

--- 1_Introduction/Practices/05_Practices/Practices_04.py
+++ 1_Introduction/Practices/05_Practices/Practices_04.py
@@ -1,3 +1,26 @@
 """
-
+Read two integer numbers A and B and inform if A is divisible by B and if it is not divisible
+print the remainder.
 """
+
+number_a = int(input("Number A = "))
+number_b = int(input("Number B = "))
+
+# Option 1
+if number_a % number_b == 0:
+    print("{0} is divisible by {1}".format(number_a, number_b))
+else:
+    print("{0} is NOT divisible by {1} and the remainder is {2}".format(number_a, number_b, number_a % number_b))
+
+# Option 2
+remainder = number_a % number_b
+if remainder == 0:
+    print("{0} is divisible by {1}".format(number_a, number_b))
+else:
+    print("{0} is NOT divisible by {1} and the remainder is {2}".format(number_a, number_b, remainder))
+
+# Option 3
+if remainder != 0:
+    print("{0} is NOT divisible by {1} and the remainder is {2}".format(number_a, number_b, remainder))
+else:
+    print("{0} is divisible by {1}".format(number_a, number_b))
