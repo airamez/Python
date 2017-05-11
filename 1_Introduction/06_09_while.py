5fb44eeb5f62d4cae5bbc67360af57fb227449c3
Jose Santos <airamez@gmail.com>
2017-05-10 22:46:33 -0700

    Fixing errors and improving the content

---
+++ 1_Introduction/06_09_while.py
@@ -0,0 +1,6 @@
+while True:
+    number = input("Type a integer number or [Enter] to exit = ")
+    if number == "":
+        break
+    number = int(number)
+    print("This is your number = ", number)
