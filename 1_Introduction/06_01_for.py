3f4dc5b01bafad14b22c305d660406c057f20e44
Jose Santos <airamez@gmail.com>
2017-05-10 19:50:32 -0700

    Fixing errors and improving the content

--- 1_Introduction/06_01_for.py
+++ 1_Introduction/06_01_for.py
@@ -1,6 +1,8 @@
 # for using range [start, end[
 for i in range(1, 10):
-    print(i)
+    print(i, end='')
+    print(', ', end='')
+    # All command aligned inside the for it part of the for looping
 
 start = 1
 end = 10
