bc1a7ad20037cd0f14fbffa12b54419264026e1d
airamez <airamez@gmail.com>
2017-05-10 22:02:30 -0700

    Bug fixes and improvements

--- 1_Introduction/Practices/05_Practices/Practices_05.py
+++ 1_Introduction/Practices/05_Practices/Practices_05.py
@@ -1,3 +1,40 @@
 """
-
+Read two integer numbers and print them in ascending order
 """
+
+number_1 = int(input("Number 1 = "))
+number_2 = int(input("Number 2 = "))
+
+# Option 1
+if number_1 < number_2:
+    print(number_1, number_2)
+else:
+    print(number_2, number_1)
+
+# Option 2
+if number_1 <= number_2:
+    print(number_1, number_2)
+else:
+    print(number_2, number_1)
+
+# Option 3
+if number_1 > number_2:
+    print(number_2, number_1)
+else:
+    print(number_1, number_2)
+
+# Option 4
+if number_1 >= number_2:
+    print(number_2, number_1)
+else:
+    print(number_1, number_2)
+
+# Option 5
+if number_2 < number_1:
+    print(number_2, number_1)
+else:
+    print(number_1, number_2)
+
+# Option 6 : if inline
+output = "{0} {1}".format(number_1, number_2) if number_1 < number_2 else "{0} {1}".format(number_2, number_1)
+print(output)
