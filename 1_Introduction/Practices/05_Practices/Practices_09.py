339b1be650c76ab059074a852102a51888e78bf5
airamez <airamez@gmail.com>
2017-05-14 23:18:51 -0700

    Bug fixes and improvements

--- 1_Introduction/Practices/05_Practices/Practices_09.py
+++ 1_Introduction/Practices/05_Practices/Practices_09.py
@@ -1,3 +1,42 @@
 """
-
+Read three values and indicate if they can form a triangle and what type of triangle it would be:
+Equilateral, Isosceles, Scalene.
 """
+side_a = float(input("Side A = "))
+side_b = float(input("Side B = "))
+side_c = float(input("Side C = "))
+
+if side_a < side_b + side_c and \
+   side_b < side_a + side_c and \
+   side_c < side_a + side_b:
+
+    # Option 1
+    print("Triangle")
+    if side_a == side_a and side_a == side_c and side_b == side_c:
+        print("Equilateral")
+    if (side_a == side_b and side_a != side_c and side_b != side_c) or \
+       (side_a == side_c and side_a != side_b and side_c != side_b) or \
+       (side_b == side_c and side_b != side_a and side_c != side_a):
+        print("Isosceles")
+    if side_a != side_b and side_a != side_c and side_b != side_c:
+        print("Scalene")
+
+    # Option 2
+    if side_a == side_a and side_a == side_c and side_b == side_c:
+        print("Equilateral")
+    elif (side_a == side_b and side_a != side_c and side_b != side_c) or \
+         (side_a == side_c and side_a != side_b and side_c != side_b) or \
+         (side_b == side_c and side_b != side_a and side_c != side_a):
+        print("Isosceles")
+    elif side_a != side_b and side_a != side_c and side_b != side_c:
+        print("Scalene")
+
+    # Option 3
+    if side_a == side_b and side_a == side_c:
+        print("Equilateral")
+    elif side_a == side_b or side_a == side_c or side_b == side_c:
+        print("Isosceles")
+    else:
+        print("Scalene")
+else:
+    print("Not Triangle")
