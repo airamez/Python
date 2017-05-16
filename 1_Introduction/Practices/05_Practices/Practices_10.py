27cffcb47c0ff106aa3138519db2635f44af01c6
airamez <airamez@gmail.com>
2017-05-15 21:58:19 -0700

    Bug fixes and improvements

--- 1_Introduction/Practices/05_Practices/Practices_10.py
+++ 1_Introduction/Practices/05_Practices/Practices_10.py
@@ -1,3 +1,20 @@
 """
-
+Generate and show two integers numbers between 0 and 99, ask the user to try the addition of both numbers
+and let the user know if the result is right or not.
 """
+import random
+
+MINIMUM = 0
+MAXIMUM = 99
+
+number1 = random.randint(MINIMUM, MAXIMUM)
+number2 = random.randint(MINIMUM, MAXIMUM)
+sum_result = number1 + number2
+
+question = "{0} + {1} = ".format(number1, number2)
+guess = int(input(question))
+
+if guess == sum_result:
+    print("Congratulations. Your answer is correct!")
+else:
+    print("Sorry. Your answer is incorrect!")
