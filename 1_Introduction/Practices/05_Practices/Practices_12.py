0e1b017c5a44e89457c988fb0bf0bed27c6bd9ef
airamez <airamez@gmail.com>
2017-05-17 23:03:32 -0700

    Bug fixes and improvements

--- 1_Introduction/Practices/05_Practices/Practices_12.py
+++ 1_Introduction/Practices/05_Practices/Practices_12.py
@@ -1,3 +1,28 @@
 """
-
+Generate an random integer number with 4 digits and generate another number with the digits in reverse order.
+    e.g.
+    Number = 1234
+    Inverted = 4321
 """
+import random
+number = random.randint(1000, 9999)
+
+number_copy = number
+
+division = divmod(number, 10)
+inverted = division[1]
+number = division[0]
+
+division = divmod(number, 10)
+inverted = inverted * 10 + division[1]
+number = division[0]
+
+division = divmod(number, 10)
+inverted = inverted * 10 + division[1]
+number = division[0]
+
+division = divmod(number, 10)
+inverted = inverted * 10 + division[1]
+
+print(number_copy)
+print(inverted)
