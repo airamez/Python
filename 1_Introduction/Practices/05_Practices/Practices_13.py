6342d20c5144ba4b28f919de62256c1ac710483d
airamez <airamez@gmail.com>
2017-05-18 22:59:55 -0700

    Bug fixes and improvements

--- 1_Introduction/Practices/05_Practices/Practices_13.py
+++ 1_Introduction/Practices/05_Practices/Practices_13.py
@@ -1,3 +1,23 @@
 """
-
+Read a time defined as Hour and Minutes where the Hour will be in military format (0 to 23) and print the time
+as conventional hour representation: Hour:Minutes AM/PM.
+    e.g.
+    Hour: 17
+    Minutes: 45
+    Conventional hour = 5:45 PM
 """
+
+hour = int(input("Hour = "))
+minutes = int(input("Minutes = "))
+
+if hour <= 11:
+    am_pm = "am"
+elif hour == 12:
+    am_pm = "pm"
+else:
+    hour -= 12
+    am_pm = "pm"
+
+print("{0}:{1}{2}".format(hour, minutes, am_pm))
+
+
