66c5cdaf835ea1db6f5707f1f7e99935da999d63
Jose Santos <airamez@gmail.com>
2017-05-22 01:03:20 -0700

    Fixing errors and improving the content

--- 1_Introduction/11_03_Set.py
+++ 1_Introduction/11_03_Set.py
@@ -1,1 +1,18 @@
+prime_numbers = {1, 2, 3, 5, 7, 11, 17, 1, 2, 3, 5}
+print(prime_numbers)
+
+prime_numbers = set()
+prime_numbers.add(1)
+prime_numbers.add(2)
+prime_numbers.add(3)
+prime_numbers.add(5)
+prime_numbers.add(7)
+prime_numbers.add(11)
+prime_numbers.add(17)
+
+prime_numbers.add(1)
+prime_numbers.add(2)
+prime_numbers.add(3)
+print(prime_numbers)
+
 # TODO: Prepare this file
