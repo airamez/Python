7007e92d3b7d944c0995987e4b58b3ca3abea9d4
Jose Santos <airamez@gmail.com>
2017-05-27 23:14:47 -0700

    Fixing errors and improving the content

--- 1_Introduction/06_Loop/06_03_for.py
+++ 1_Introduction/06_Loop/06_03_for.py
@@ -1,5 +1,3 @@
-for i in range(30, 0, -1):
-    if i > 1:
-        print("{0}, ".format(i), end="")
-    else:
-        print(i)
+for i in range(0, 50, 5):
+    print(i)
+print()
--- 1_Introduction/06_Loop/06_04_for.py
+++ 1_Introduction/06_Loop/06_04_for.py
@@ -1,3 +1,2 @@
-for i in range(0, 100, 5):
-    print(i)
-print()
+for i in range(20, -20, -1):
+        print(i)
