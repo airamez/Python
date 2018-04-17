f5c19d028477e20438d2e7ba088d63776035c98d
Jose Santos <airamez@gmail.com>
2018-04-16 17:16:37 -0700

    New content

--- 1_Introduction/06_Looping/06_08_for.py
+++ 1_Introduction/06_Looping/06_08_for.py
@@ -2,9 +2,17 @@
 # continue = skip the current interaction
 # break = ends the loop
 
-for i in range(1, 100):
+print('continue example')
+for i in range(1, 10):
+    if 4 < i < 8:
+        continue
+    print(i)
+
+print('continue and break example')
+for i in range(1, 1000):
     if i == 2 or i == 4:
         continue
     if i == 5:
         break
     print(i)
+print('After the for command')
