ab4469c3f1ca7c4ecf7f00e213d454cbe515e651
Jose Santos <airamez@gmail.com>
2018-04-28 18:08:12 -0700

    Looping Exercise 01 resolution.

--- 1_Introduction/06_Looping/Practices/Practices_01.py
+++ 1_Introduction/06_Looping/Practices/Practices_01.py
@@ -1,3 +1,11 @@
 """
-
+01. Print the numbers from 1 to 1000 without using new lines after each number.
 """
+for number in range(1, 1001):
+    print(str(number) + ' ', end='')
+
+print()
+
+for number in range(1, 1001):
+    print("{0} ".format(number), end='')
+
