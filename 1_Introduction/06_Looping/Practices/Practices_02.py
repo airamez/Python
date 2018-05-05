d6b4ac65e94dd8520ee0c9a77c748704bf9acae4
Jose Santos <airamez@gmail.com>
2018-05-04 23:10:19 -0700

    Looping Exercise 01 resolution.

--- 1_Introduction/06_Looping/Practices/Practices_02.py
+++ 1_Introduction/06_Looping/Practices/Practices_02.py
@@ -1,3 +1,13 @@
 """
-
+02. Read two integer numbers representing an interval and print all numbers inside the interval.
 """
+
+start = int(input('Start = '))
+end = int(input('End = '))
+
+if start < end:
+    for n in range(start + 1, end):
+        print(str(n) + ' ', end='')
+else:
+    for n in range(start - 1, end, -1):
+        print(str(n) + ' ', end='')
