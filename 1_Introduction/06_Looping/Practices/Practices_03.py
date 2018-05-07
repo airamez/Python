6fccd002a453fd11f9d66c4f71ea828e0f81488f
Jose Santos <airamez@gmail.com>
2018-05-06 22:07:51 -0700

    Looping Exercise 02 and 03 resolution.

--- 1_Introduction/06_Looping/Practices/Practices_03.py
+++ 1_Introduction/06_Looping/Practices/Practices_03.py
@@ -1,3 +1,32 @@
 """
-
+03. Read two integer numbers representing an interval and print all even numbers inside the interval.
 """
+
+start = int(input('Start = '))
+end = int(input('End = '))
+
+# Solution 1
+increment = 1
+if start > end:
+    increment = -1
+for n in range(start + increment, end, increment):
+    if n % 2 == 0:
+        print(str(n) + ' ', end='')
+print()
+
+# Solution 2
+if start < end:
+    increment = 2
+    if start % 2 == 0:
+        start += 2
+    else:
+        start += 1
+else:
+    increment = -2
+    if start % 2 == 0:
+        start -= 2
+    else:
+        start -= 1
+
+for n in range(start, end, increment):
+    print(str(n) + ' ', end='')
